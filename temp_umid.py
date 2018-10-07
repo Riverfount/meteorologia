17d93a524829a1c302845923169a557849c44329
Vicente Marçal <vicente.marcal@gmail.com>
2018-10-07 15:19:49 -0300

    Protected sensitive data

--- temp_umid.py
+++ temp_umid.py
@@ -2,12 +2,16 @@ import Adafruit_DHT as dht
 import RPi.GPIO as gpio
 import psycopg2
 import requests
+from decouple import config
 
 sensor = dht.DHT11
 gpio.setmode(gpio.BOARD)
 pino_sensor = 4
 url = 'https://api.thingspeak.com/update'
-api_key = 'UUOSB2RRTG29Q0J1'
+api_key = config('API_KEY')
+
+
+umid, temp = dht.read_retry(sensor, pino_sensor)
 
 connect = psycopg2.connect(
     host='192.168.0.106',
@@ -17,11 +21,7 @@ connect = psycopg2.connect(
 )
 cursor = connect.cursor()
 
-umid, temp = dht.read_retry(sensor, pino_sensor)
-
-sql = f"""
-    INSERT INTO dados_temp_umid (temperatura, umidade)
-    VALUES ({temp:.2f}, {umid:.2f})"""
+sql = f"""INSERT INTO dados_temp_umid (temperatura, umidade) VALUES ({temp}, {umid})"""
 cursor.execute(sql)
 connect.commit()
 connect.close()
